61307b04947043f2afa35eba2a3e9c514be1e474
Michael Chierici <michael.chierjr@gmail.com>
2022-07-21 22:45:00 -0300

    feat(cors): include cors in index

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response, Router } from "express";
 import { Routes } from "./src/routes/routes";
 import { createConnection } from "typeorm";
+
 import "reflect-metadata";
 
 export class AppController {
   static run() {
+    const cors = require("cors");
     const app = express();
     createConnection();
     app.use(express.json());
+    app.use(cors());
     app.get("/", (req: Request, res: Response) => {
       res.send("hello world");
     });
